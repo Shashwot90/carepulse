eb15b379ace5f227f1fe5353a1ead09151071b77
unknown <shashwotpradhan@gmail.com>
2024-07-19 14:09:59 +0545

    cn

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Plus_Jakarta_Sans } from "next/font/google";
 import "./globals.css";
+import { cn} from '/@lib/utils'
+
 
 const fontSans = Plus_Jakarta_Sans({ 
   subsets: ["latin"],
@@ -19,7 +21,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={cn}>{children}</body>
     </html>
   );
 }
